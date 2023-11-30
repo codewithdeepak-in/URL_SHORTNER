6aa4bf98e6f8b87cf7f78f760ee3f293eed72bc1
codewithdeepak-in <bagpacker.3778@gmail.com>
2023-11-30 22:18:32 +0530

    commit

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
+const URL = require('./models');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyparser.json());
-app.use('/' , routes);
+app.use('/api' , routes);
+app.set('views', './views');
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
 
 
 
 
 
-
-
+app.get('/', async (req, res) => {
+    const data =  await URL.find();
+    res.render("home", {data});
+})
 
 
 // server starting.
